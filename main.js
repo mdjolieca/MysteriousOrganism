9701e693b92723e0c240cdefd84d90a9a2f89820
Marie-Denise Jolie <mdjolieca@gmail.com>
2021-02-07 14:48:40 -0500

    Create main.js

---
+++ main.js
@@ -0,0 +1,73 @@
+// Returns a random DNA base
+const returnRandBase = () => {
+  const dnaBases = ['A', 'T', 'C', 'G']
+  return dnaBases[Math.floor(Math.random() * 4)] 
+}
+
+// Returns a random single stand of DNA containing 15 bases
+const mockUpStrand = () => {
+  const newStrand = []
+  for (let i = 0; i < 15; i++) {
+    newStrand.push(returnRandBase())
+  }
+  return newStrand
+}
+
+const pAequorFactory = (specimenNum, dna) => {
+   return {
+      specimenNum : specimenNum,  dna: dna,
+      compareDNA (pAequor) {
+        let count = 0;
+        for(let i = 0 ; i< this.dna.length ; i++) {
+           if(this.dna[i]=== pAequor.dna[i]) {
+             count++;
+           }
+        }
+        let percentage = Math.round((count/this.dna.length) * 100);
+        console.log(`specimen #${this.specimenNum} and specimen #${pAequor.specimenNum} have ${percentage}% DNA in common`);
+     },
+
+    mutate() {
+    let  baseToMutate = Math.floor(Math.random()* this.dna.length-1);
+    let newBase ; 
+      do{//mutation base must be different mutated base
+           newBase = returnRandBase();
+        } while (this.dna[baseToMutate] === newBase)
+     this.dna[baseToMutate] = newBase;
+    return this.dna;
+   },
+   willLikelySurvive(){
+     let count = 0;
+        for(let i = 0 ; i< this.dna.length ; i++) {
+           if(this.dna[i]=== 'C' || this.dna[i]==='G') {
+             count++;
+           }
+        }
+        let percentage = (count/this.dna.length) * 100;
+        return percentage >= 60;
+   }
+
+   }
+}
+
+const pAequors = [];
+for(let j=0; j<30;j++) {
+  pAequors.push(pAequorFactory(j,mockUpStrand()) );
+}
+
+pAequors.forEach( pAequor =>{
+  console.log('specimenNum: ' +pAequor.specimenNum);
+  console.log('DNA:         ' + pAequor.dna);
+  console.log('Mutated DNA: ' +pAequor.mutate());
+  console.log('Likely to Survive: ' +pAequor.willLikelySurvive());
+});
+
+for(let j=1; j<30;j++) {
+   pAequors[j-1].compareDNA(pAequors[j]);
+}
+
+
+
+
+
+
